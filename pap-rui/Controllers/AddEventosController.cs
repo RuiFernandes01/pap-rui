375540d591219746a56dcf182eead22c111a7c4b
piFernandes94 <60491320+piFernandes94@users.noreply.github.com>
2020-04-05 17:08:57 +0100

    Adicionar funcionalidade de adicionar eventos

--- pap-rui/Controllers/AddEventosController.cs
+++ pap-rui/Controllers/AddEventosController.cs
@@ -8,10 +8,27 @@ namespace pap_rui.Controllers
 {
     public class AddEventosController : Controller
     {
-        // GET: AddEventos
+        private iluminarteEntities db = new iluminarteEntities();
         public ActionResult Index()
         {
-            return View("/Views/dashboard/addEvento.cshtml");
+            if (Session["login"] != null)
+            {
+                return View("/Views/dashboard/addEvento.cshtml");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+
+
+        public ActionResult adicionarEventos(eventos teste)
+        {
+
+            db.eventos.Add(teste);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "dashboard");
         }
     }
 }
