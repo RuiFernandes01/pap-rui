2a06e30019cd84c0fe1c7e0c3e858e2af89b5eab
RuiFernandes01 <63199206+RuiFernandes01@users.noreply.github.com>
2020-05-31 17:02:30 +0100

    mapa + favicon

--- pap-rui/includes/js/functions.js
+++ pap-rui/includes/js/functions.js
@@ -1,11 +1,18 @@
 ﻿$(document).ready(function () {
-
-
     $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 10,
         nav: true,
         items: 1,
     })
-})
 
+    var map = L.map('mapid').setView([37.138957, -8.021973], 14);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([37.138957, -8.021973]).addTo(map)
+        .bindPopup('Academia Iluminarte')
+        .openPopup();
+})
