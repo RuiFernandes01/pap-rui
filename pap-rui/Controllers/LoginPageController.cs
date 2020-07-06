18d03ccbb098d13b7e5c3fb92cf4d1ec8a2b235d
RuiFernandes01 <63199206+RuiFernandes01@users.noreply.github.com>
2020-07-06 18:13:55 +0100

    LoginPage e base de dados

---
+++ pap-rui/Controllers/LoginPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pap_rui.Controllers
+{
+    public class LoginPageController : Controller
+    {
+        // GET: LoginPage
+        public ActionResult Index()
+        {
+            return View("/Views/Login/LoginPage.cshtml");
+        }
+    }
+}
---
+++ pap-rui/Controllers/SignupController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pap_rui.Controllers
+{
+    public class SignupController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("/Views/Signup/signup.cshtml");
+        }
+    }
+}
